a05edf9570a3560e808eb61117f554740baa718a
ESTRELLA MANCHA ROSETE <148529673+MANCHROSE@users.noreply.github.com>
2024-04-04 00:24:28 +0800

    Add files via upload

---
+++ PHP-ACT-1.1.php
@@ -0,0 +1,20 @@
+<?php
+//Declare variables of different data types
+$stringVar="Hi dear,";
+$integerVar=10;
+$floateVar=5.5;
+$booleanVar=true;
+
+//perform arithmetic operations
+$sum=$integerVar+$floateVar;
+$difference=$floateVar-$integerVar;
+$product=$integerVar*$floateVar;
+$quotient=$floateVar/$integerVar;
+
+//Ouput the result
+echo $stringVar. "The sum is: ". $sum. "<br>";
+echo $stringVar. "The difference is: ". $difference. "<br>";
+echo $stringVar. "The product is: ". $product. "<br>";
+echo $stringVar. "The quotient is: ". $quotient. "<br>";
+
+?>
