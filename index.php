46472b80312ab42f7119b60900b96020329f01fc
athenaquinit12 <148975069+athenaquinit12@users.noreply.github.com>
2024-05-09 16:52:27 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,38 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php       
-       if(isset($_POST['submit'])){
-            
-            $firstname = $_POST['name'];
-            $age        = $_POST['age'];
+<?php
+    $servername = "localhost";
+    $username = "admin";
+    $password = "admin";
+    $dbname = "act-5";
 
-            echo $firstname . " you are " . $age . ' years old.';
-       }
-       
-    ?>
-    <form action="index.php" method="POST">
-        <table>
-            <tr>
-                <td>Name</td>
-                <td><input name="name" type="text" /> </td>
-            </tr>
-            <tr>
-                <td>Age</td>
-                <td><input name="age" type="text" /> </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input name="submit" type="submit" value="Submit" /> </td>
-            </tr>
-        </table>
-    </form>
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-</body>
-</html>
+?>
