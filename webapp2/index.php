416377bbaa74718771c8ce1f14f3b3a776c56238
jastene-ariate <148976679+jastene-ariate@users.noreply.github.com>
2024-05-10 15:31:41 +0800

    Add files via upload

---
+++ webapp2/index.php
@@ -0,0 +1,9 @@
+<?php
+    $servername = "localhost";
+    $username = "admin";
+    $password = "admin";
+    $dbname = "act-5";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
